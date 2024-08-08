d6f487ffbc631a12b6ed87680462533c17a04bc1
Gokulcasper <ronitcasper215@gmail.com>
2024-08-08 20:43:50 +0530

    Commit Task Completed

---
+++ index.js
@@ -0,0 +1,40 @@
+// let saveBtn = document.getElementById("saveBtn");
+// let clearBtn = document.getElementById("clearBtn");
+// let userInput = document.getElementById("userInput");
+
+// function saveData(){
+//     let userValue = userInput.value;
+//     console.log(userValue);
+//     localStorage.setItem("userValue",userValue);
+// };
+// function removeData(){
+//     userInput.value='';
+//     localStorage.removeItem('userValue');
+// }
+// saveBtn.addEventListener("click",saveData);
+// clearBtn.addEventListener("click",removeData);
+
+// --------- Local Storage Practice ------------
+
+// let msg = document.getElementById("msg");
+// let saveButton = document.getElementById("saveBtn");
+// let clearButton = document.getElementById("clearBtn");
+// function saveMsg(){
+//     let textAreaValue = msg.value;
+//     console.log(textAreaValue)
+//     localStorage.setItem('textAreaValue',textAreaValue);
+// };
+// function removeMsg(){
+//     msg.value = '';
+//     localStorage.removeItem('textAreaValue');
+// };
+// document.addEventListener('DOMContentLoaded',function(){
+//     if(localStorage.getItem('textAreaValue')){
+//         msg.value=localStorage.getItem('textAreaValue');
+//     }
+// });
+// saveButton.addEventListener("click",saveMsg);
+// clearButton.addEventListener("click",removeMsg);
+
+// ---------- ChatBot ----------
+
